ff70bcd62d1ad2a2d419af59438249d73f861556
hidden-r3d <75956940+hidden-r4d@users.noreply.github.com>
2021-02-07 03:39:55 -0800

    nigmode

---
+++ NIGMODEHAXXv2.py
@@ -0,0 +1,27 @@
+import time
+import requests
+import webbrowser
+
+
+ip = requests.get("https://api.ipify.org").text
+y = 20
+
+print("\033[31m" + "NIGMODE HAXX V2")
+question = input("Type 1 to start: ")
+if question == "1":
+ print("Loading NIGMODE HAxx V2...")
+time.sleep(2)
+print("Checking...")
+time.sleep(2)
+print("Organizing files...")
+time.sleep(2)
+print("Installing", y, "Modules...")
+time.sleep(5)
+print("Request blocked by firewall.")
+time.sleep(1)
+print("Attempting to Bypass firewall...")
+time.sleep(10)
+print("Attempt Failed!")
+time.sleep(0.5)
+print("Sorry, but the tool you are using is ILLEGAL and your IP address (",ip,") has been sent to the CIA and MOSSAD for further investigation.") 
+webbrowser.open_new("https://joinis.is")
---
+++ nointernet_hack_hack_anonymoose.bat
@@ -0,0 +1,88 @@
+@echo off
+color 2
+echo Epic hcakcerman mans hacks loading...
+timeout 1 >nul
+color 3
+title you
+timeout 1 >nul
+color 5
+title are
+timeout 1 >nul
+color 6
+title fucked
+timeout 1 >nul
+color 3
+title lmao
+timeout 1 >nul
+color 5
+title EPic hacks anonymouse ofical hacks virgin 3.5
+timeout 1 >nul
+color 6
+timeout 1 >nul
+color 2
+echo ===============================================================================================
+echo                         Haxxor man skidded hacks DDoser vershion 6.9 ready!                                                                                                                                           
+echo ===============================================================================================
+echo Haxxor man skidded hacks DDoser ready!
+timeout 3 >nul
+echo hakced into US government satalite
+timeout 2 >nul
+color 4
+echo ::[ERROR]:: Log: Firewall blocked epic hax injektor!!1
+timeout 2 >nul
+color 1
+echo ::BYPASSING FIREWALL...::
+timeout 2 >nul
+echo Injecting hackerman exploit packets into fyrewal...
+timeout 2 >nul
+echo stealing your credit card information..
+timeout 2 >nul
+echo {hack} {hack} manifest: DDOS
+timeout 2 >nul
+echo {machine response: you hack?} {response: Yes.}
+timeout 2 >nul
+echo {Nointernet: yes confirm hack hack hack}
+timeout 2 >nul
+echo confirm: ipv4_grabb ? {yes} {ip : 127.0.0.1} DDOS HACK {hack} [hack]
+timeout 2 >nul
+echo manifest: computer hack DDOS ip grab epic chungus fortnite keanu 100 epic[]{}
+timeout 2 >nul
+echo finishing hax...
+timeout 5 >nul
+color 2
+echo Epic haxxes complete!!1
+timeout 2 >nul
+echo also we sold all your info to israel
+timeout 3 >nul
+echo and iur harddrive gona get wiped in 3
+timeout 1 >nul
+echo 2..
+timeout 1 >nul
+echo 1..
+timeout 1 >nul
+title yes epic hacker ip grab ddos machine responde are u hack me yes yes yeys
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+color 4
+tree /f
